9c815f44ba2ff990f04a87b37398cc5c16614d7d
github username <github email address>
2018-12-13 11:56:24 -0500

    Success fetching from 4SQ API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import MainContainer from './MainContainer'
 import './App.css';
+import Venues from './Venues/index'
+
 
 class App extends Component {
+  handleRoute = event => {console.log(event)}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <Venues/>
+
       </div>
     );
   }
 }
 
 export default App;
+//App will Render these components: MainContainer,Navbar, landing page,
---
+++ src/MainContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainContainer extends Component {
+  render() {
+    return (
+      <div >
+        This is MainContainer component
+      </div>
+    );
+  }
+}
+
+export default MainContainer;
